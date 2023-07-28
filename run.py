a441cf63489121a2b75d00830723edbcca5205b9
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2023-07-28 14:30:03 +0200

    update

--- run.py
+++ run.py
@@ -1,18 +1,11 @@
 general_text = {
     'title': 'Teil in FreeCAD Part Design entwerfen',
-    'topic_intro': 'In dieser Anleitung werden wir ein Teil mit FreeCAD entwerfen. Das Teil wird in einem Produkt verwendet und hat die Funktion etwas zu tun. Das produkt wird in der Industrie verwendet um etwas zu tun. Das Teil aus dieser Anleitung wird mittels eines Verfahrens hergestellt.',
+    'topic_intro': 'In dieser Anleitung werden wir ein Teil mit FreeCAD entwerfen. Das Teil wird in einem Produkt verwendet und hat die Funktion etwas zu tun. Das Produkt wird in der Industrie verwendet um etwas zu tun. Das Teil aus dieser Anleitung wird mittels eines Verfahrens hergestellt.',
     'steps_intro': 'In mehreren Schritten werden wir einige Part Design Features und Skizzen verwenden. Lasst uns mit dem ersten Schritt beginnen:',
     'success': 'Glückwunsch, du hast es geschafft!',
     'summary': 'In dieser Anleitung haben wir die Part Design Features und Skizzen angewendet.',
     'next': 'Als Nächstes entwerfe die anderen Teile des Produktes.',
     'ad_upsell': 'Mache eine Schulung um zu lernen, wie du FreeCAD in einer industriellen Umgebung zuverlässig anwenden kannst. Die FreeCAD Schulung zeigt dir, wie du mit FreeCAD in deinen Entwicklungsprozess im Maschinenbau integrieren kannst. Buche noch heute einen Termin vor Ort oder online.'
-}
-
-feature_text = {
-    'Sketcher::SketchObject': 'Erstelle eine Skizze.',
-    'PartDesign::Pad': 'Wähle die zuletzt erstellte Skizze und erstelle eine Aufpolsterung.',
-    'PartDesign::Pocket': 'Wähle die zuletzt erstellte Skizze und erstelle eine Tasche.',
-    'PartDesign::PolarPattern': 'Wähle das zuletzt erstellte Feature und erstelle ein Polares Muster.'
 }
 
 class ManualGenerator():
@@ -63,8 +56,9 @@ if __name__ == '__main__':
     import sys
     import os
 
-    #FREECAD_ABS_PATH = 'C:/PROGRA~1/FreeCAD_weekly-builds-29126-2022-06-12-conda-Windows-x86_64-py38/bin'
-    FREECAD_ABS_PATH = 'C:/PROGRA~1/FreeCAD 0.20/bin'
+    #FREECAD_ABS_PATH = 'C:/PROGRA~1/FreeCAD_weekly-builds-29126-2022-06-12-conda-Windows-x86_64-py38/bin' 
+    FREECAD_ABS_PATH = 'C:/PROGRA~1/FreeCAD_weekly-builds-29126-2022-06-12-conda-Windows-x86_64-py38/bin'
+    #FREECAD_ABS_PATH = 'C:/PROGRA~1/FreeCAD 0.20/bin'
 
     # Add path to FreeCAD Python interface
     # path to your FreeCAD.so or FreeCAD.dll file
@@ -80,9 +74,13 @@ if __name__ == '__main__':
     # setup project paths
     module_abs_path = os.path.abspath(__file__)
     module_abs_dir_path = os.path.dirname(module_abs_path)
+    filename_no_ext = 'Elektroblech'
+    path_fcstd = os.path.join(module_abs_dir_path, f'{filename_no_ext}.FCStd')
+    path_html = os.path.join(module_abs_dir_path, f'{filename_no_ext}.html')
+    path_pdf = os.path.join(module_abs_dir_path, f'{filename_no_ext}.pdf')
 
     # load the FreeCAD document
-    doc = FreeCAD.openDocument(os.path.join(module_abs_dir_path, 'Elektroblech.FCStd'))
+    doc = FreeCAD.openDocument(path_fcstd)
 
     # create a new manual generator object with a body from the loaded FreeCAD document
     mg = ManualGenerator(doc.Body)
@@ -93,11 +91,9 @@ if __name__ == '__main__':
     steps = []
 
     for feature in body_features:
-        steps.append(feature_text[feature.TypeId])
-        print(feature.TypeId)
+        steps.append(feature.TypeId)
 
     import jinja2
-    #import pdfkit
 
     context = {'general_text': general_text, 'steps': steps}
 
@@ -108,11 +104,18 @@ if __name__ == '__main__':
     template = env.get_template('template.html')
     output_text = template.render(context)
 
-    #from bs4 import BeautifulSoup
-    #print(BeautifulSoup(output_text, 'html.parser').prettify())
+    #save output_text to html file located in same folder as used freecad file
+    with open(path_html, 'w') as file:
+        file.write(output_text)
 
-    print(output_text)
+    #convert html file with instruction to pdf file and save it in the same folder as the html and freecad file
+    import pdfkit
 
+    path_wkhtmltopdf = r'C://Program Files//wkhtmltopdf//bin//wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
 
+    pdfkit.from_file(path_html, path_pdf, configuration=config)
+
+    
     
     
